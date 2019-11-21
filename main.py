5a2daaaad4a46ebf75da5828194dd0fa06a9cbeb
Bradley Small <vc.Bradley.Small@lowes.com>
2019-11-21 11:28:47 -0500

    changing for vimdiff

--- main.py
+++ main.py
@@ -2,23 +2,34 @@
 def NFact135():
     def fact(x):
         _fact = 1
-        for i in range(1, x+1):
+        for i in range(1, x + 1):
             _fact = _fact * i
         return _fact
+
     #        Leaps         Ones      threes    fives
-    return (fact(10) // ( fact(10) * fact(0) * fact(0) ) +
-            fact(8)  // ( fact( 7) * fact(1) * fact(0) ) +
-            fact(6)  // ( fact( 4) * fact(2) * fact(0) ) +
-            fact(6)  // ( fact( 5) * fact(0) * fact(1) ) +
-            fact(4)  // ( fact( 1) * fact(3) * fact(0) ) +
-            fact(4)  // ( fact( 2) * fact(1) * fact(1) ) +
-            fact(2)  // ( fact( 0) * fact(0) * fact(2) ) )
+    return (
+        fact(10) // (fact(10) * fact(0) * fact(0))
+        + fact(8) // (fact(7) * fact(1) * fact(0))
+        + fact(6) // (fact(4) * fact(2) * fact(0))
+        + fact(6) // (fact(5) * fact(0) * fact(1))
+        + fact(4) // (fact(1) * fact(3) * fact(0))
+        + fact(4) // (fact(2) * fact(1) * fact(1))
+        + fact(2) // (fact(0) * fact(0) * fact(2))
+    )
+
+
+def TESTFUNC():
+    a = 7
+    for i in range(10):
+        print(i + a)
+
+    return 0
 
 
 def NFact(x):
     def fact(x):
         _fact = 1
-        for i in range(1, x+1):
+        for i in range(1, x + 1):
             _fact = _fact * i
         return _fact
 
@@ -27,30 +38,31 @@ def NFact(x):
         ones = x - (2 * twos)
         if ones < 0:
             return 0
-        return r_NFact(num - 1) + ( fact(num) // (fact(ones) * fact(twos)))
+        return r_NFact(num - 1) + (fact(num) // (fact(ones) * fact(twos)))
+
     return r_NFact(x)
 
 
 def N(x):
-    if (x < 0):
+    if x < 0:
         return 0
-    if (x == 0 or x == 1):
+    if x == 0 or x == 1:
         return 1
-    return N(x-1) + N(x-2)
+    return N(x - 1) + N(x - 2)
 
 
 def N135(x):
-    if (x < 0):
+    if x < 0:
         return 0
-    if (x == 0):
+    if x == 0:
         return 1
-    return (N135(x-1) + N135(x-3) + N135(x-5))
+    return N135(x - 1) + N135(x - 3) + N135(x - 5)
 
 
 def NArr(x, arr):
-    if (x < 0):
+    if x < 0:
         return 0
-    if (x == 0):
+    if x == 0:
         return 1
     return sum([NArr(x - i, arr) for i in arr])
 
@@ -58,17 +70,17 @@ def NArr(x, arr):
 def main():
     print(NFact135())
 
-    for n in range(10+1):
+    for n in range(10 + 1):
         print(n, NFact(n))
 
-    for n in range(10+1):
+    for n in range(10 + 1):
         print(n, N(n))
 
-    for n in range(10+1):
+    for n in range(10 + 1):
         print(n, N135(n))
 
-    for n in range(10+1):
-        print(n, NArr(n, [1,3,5]))
+    for n in range(10 + 1):
+        print(n, NArr(n, [1, 3, 5]))
 
 
 if __name__ == "__main__":
